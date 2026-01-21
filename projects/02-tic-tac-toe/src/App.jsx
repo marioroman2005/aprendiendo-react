9a05c8f96dbdee6d51ffd3cafea5bcaa97514495
marioroman2005 <mariorota2005@gmail.com>
2026-01-21 14:44:24 +0100

    Min 30, acabamos funcionalidad de turnos y encontar ganadores

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -6,22 +6,69 @@ const TURNS = {
   O : 'o'
 }
 
-const Square = ({ children, isSelected, updateBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? 'is-selected' : ''}`
   
+  const handleClick = () => {
+    updateBoard(index)
+  }
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 function App() {
   const [board, setBoard] = useState(
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null es que no hay ganador, false es que hay un empate
 
+
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras 
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS){
+      const [a, b, c] = combo
+      if(
+        boardToCheck[a] && // 0 -> x u o
+        boardToCheck[a] === boardToCheck[b] && // 0 y 3 -> x -> x u o
+        boardToCheck[a] === boardToCheck[c] // si para todas las posiciones ganadoras son todo x o todos o, hay ganador
+      ){
+        return boardToCheck[a] // x u o
+      }
+    }
+    return null
+
+  }
+
+  const updateBoard = (index) => {
+
+    // no actualizamos esta posicion si ya tiene algo
+    if(board[index]) return
+    
+    // actualizar el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn // x u o
+    setBoard(newBoard)
+    
+    // cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
 
   return (
@@ -34,6 +81,7 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
