f41edc4b69bbc98f3f65e604090a62483c45b0de
marioroman2005 <mariorota2005@gmail.com>
2026-01-25 19:53:31 +0100

    Min 1:35, limpiando efectos, casi acabado el tercer proyecto

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
   
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+  
+  useEffect(() => {
+    console.log('effect', { enabled } )
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition( { x: clientX, y: clientY})
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // se ejecuta cuando react deja de renderizar el componente para limpiar, y cada vez que cambie la dependencia
+    return () => { 
+      window.removeEventListener('pointermove', handleMove)
+    }
+    
+  }, [enabled])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <main>
+        <div style={{
+          position : 'absolute',
+          backgroundColor : 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius : '50%',
+          opacity : 0.8,
+          pointerEvents : 'none',
+          left : -25,
+          top: -25,
+          width: 40,
+          height: 40,
+          transform : `translate(${position.x}px, ${position.y}px)`
+        }}
+        />
+        <button onClick={() => setEnabled(!enabled)}>
+          {enabled ? 'Desactivar' : 'Activar'} Seguir puntero
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </main>
+      
     </>
   )
 }
