7c248639916ae0868f4c804674205c41962260d4
marioroman2005 <mariorota2005@gmail.com>
2026-01-25 18:58:44 +0100

    commit de lint

--- projects/02-tic-tac-toe/src/components/Square.jsx
+++ projects/02-tic-tac-toe/src/components/Square.jsx
@@ -4,7 +4,7 @@ export const Square = ({ children, isSelected, updateBoard, index }) => {
     
     const handleClick = () => {
     updateBoard(index)
-    }
+    } 
     return (
     <div onClick={handleClick} className={className}>
         {children}
