4f62e5d28404e7745db59803fab5968efa0aea83
marioroman2005 <mariorota2005@gmail.com>
2026-02-04 13:38:43 +0100

    Proyecto finalizado

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,12 +1,91 @@
-
 import './App.css'
+import { useMovies } from './hooks/useMovies.js'
+import { Movies } from './components/Movies.jsx'
+import { useState, useMemo } from 'react'
+import debounce from 'just-debounce-it'
+
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+
+  const setSearch = (newSearch) => {
+    if (newSearch.startsWith(' ')) return
+    updateSearch(newSearch)
+
+    if (newSearch === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+
+    if (newSearch.match(/^\d+$/)) {
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if (newSearch.length < 3) {
+      setError('La búsqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }
+
+  return { search, updateSearch: setSearch, error }
+}
 
 function App() {
+  const [sort, setSort] = useState(false)
+
+  const { search, updateSearch, error } = useSearch()
+  const { movies, loading, getMovies } = useMovies({ search, sort })
+
+  const debouncedGetMovies = useMemo(() =>
+    debounce(search => {
+      console.log('search', search)
+      getMovies({ search })
+    }, 300)
+    , [getMovies]
+  )
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    getMovies({ search })
+  }
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedGetMovies(newSearch)
+  }
 
   return (
-    <>
-      <h1>Prueba tecnica</h1>
-    </>
+    <div className='page'>
+
+      <header>
+        <h1>Buscador de películas</h1>
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...'
+          />
+          <input type='checkbox' onChange={handleSort} checked={sort} />
+          <button type='submit'>Buscar</button>
+        </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+      </header>
+
+      <main>
+        {
+          loading ? <p>Cargando...</p> : <Movies movies={movies} />
+        }
+      </main>
+    </div>
   )
 }
 
---
+++ projects/05-react-buscador-peliculas/src/components/Movies.jsx
@@ -0,0 +1,33 @@
+function ListOfMovies({ movies }) {
+    return (
+        <ul className="movies">
+            {
+                movies.map(movie => (
+                    <li className="movie" key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <p>{movie.year}</p>
+                        <img src={movie.image} alt={movie.title} />
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
+
+function NoMovieResults() {
+
+    return (
+        <p>No se han encontrado peliculas</p>
+    )
+}
+
+export function Movies({ movies }) {
+
+    const hasMovies = movies?.length > 0
+
+    return (
+        hasMovies
+            ? <ListOfMovies movies={movies} />
+            : <NoMovieResults />
+    )
+}
